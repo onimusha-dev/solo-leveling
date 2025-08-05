b0719787a63259ca72376b075d0def024a7c4c40
onimusha-dev <musaddikulislam007@gmail.com>
2025-08-05 10:32:38 +0530

    first commit

---
+++ apps/app/server/src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
