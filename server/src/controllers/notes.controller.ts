43d65370d142939f6eb26f48d83394999a2fcb21
onimusha-dev <musaddikulislam007@gmail.com>
2025-08-16 00:08:00 +0530

    .

---
+++ server/src/controllers/notes.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+
+interface requestBody {
+    title: string;
+    body: string;
+    tags: string[];
+}
+
+interface resBody {
+    success: boolean;
+    status: number;
+    message: string;
+    data: requestBody;
+}
+
+
+
+const createNote = (req: Request<{}, resBody,requestBody>, res: Response<resBody>) => {
+    try {
+            const { title, body, tags } = req.body
+
+            console.log(title, body, tags)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export default {
+    createNote
+}
